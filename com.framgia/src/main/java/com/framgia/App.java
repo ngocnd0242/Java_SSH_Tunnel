95b724814610e3f7d6aa6a2045e57b1c413a007d
ngo.dinh.ngoc <ngo.dinh.ngoc@framgia.com>
2018-06-25 22:04:05 +0700

    init

---
+++ com.framgia/src/main/java/com/framgia/App.java
@@ -0,0 +1,15 @@
+package com.framgia;
+
+import com.chilkatsoft.CkSshTunnel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
